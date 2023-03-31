4d841e7d11896960fb12ec98dccaa92436d0f9c9
MBANK\purev-erdene.ch <purev-erdene.ch@m-bank.mn>
2023-03-31 15:51:44 +0800

    movie page ui create

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -1,12 +1,36 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:song/model/movie/index.dart';
 
-class MovieCart extends StatelessWidget {
-  const MovieCart({super.key});
+class MovieCard extends StatelessWidget {
+  final MovieModel data;
+
+  const MovieCard(this.data, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final width = MediaQuery.of(context).size.width / 3 - 20;
+    return Column(
+      children: [
+        Container(
+          height: width * 1.5,
+          width: width,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            image: DecorationImage(image: NetworkImage(data.imgUrl), fit: BoxFit.fill),
+          ),
+        ),
+        SizedBox(height: 5),
+        SizedBox(
+          width: width,
+          child: Text(
+            data.title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.7),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
