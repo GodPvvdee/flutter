4b96f51ccd225d34f15f5242fe295b3051ddf17a
MBANK\purev-erdene.ch <purev-erdene.ch@m-bank.mn>
2023-03-31 09:48:26 +0800

    login page ui done

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:song/screens/login.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,10 +11,9 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  bool _isLogged = false;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Login page"),
-    );
+    return LoginPage();
   }
 }
